99bcae3053a06f908236702ae26fed4627605f88
torielll <anderman_tor@mail.ru>
2018-05-02 22:02:11 +0300

    final

--- webpak.config.js
+++ webpak.config.js
@@ -4,4 +4,14 @@ plugins: [
       jQuery: 'jquery',
       'window.jQuery': 'jquery'
     }),
+    
 ],
+const path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
